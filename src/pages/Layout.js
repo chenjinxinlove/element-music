5aee6501b3b6b7360ac9d4992afdcb7e4dad7597
chenjinxinlove <505150704@qq.com>
2017-12-17 20:57:19 +0800

    layout

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
+import injectSheet from 'react-jss';
 import {observer} from 'mobx-react';
+import Layout from './Layout';
+
+const classes = {
+  container: {
+    position: 'fixed',
+    left: 0,
+    top: 0,
+    width: '100vw',
+    height: '100vh'
+  },
+
+  mask: {
+    filter: 'blur(10px)'
+  }
+};
 
 @observer
-export default class Layout extends Component {
+class Layout extends Component {
   render() {
     return (
-      <div>
-        <div style={{
-          height: 'calc(100vh - 100px)',
-          overflow: 'hidden',
-          overflowY: 'auto',
-          background: `rgba(255,255,255,.8)`
-        }}>
-          {this.props.children}
-        </div>
+      <div className={classes.container} ref="container">
+        {this.props.children}
       </div>
     );
   }
 }
+
+export default injectSheet(classes)(Layout);
